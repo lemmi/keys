e6339e9bc2a0ab612fff4eb741848404717184f3
lemmi <lemmi@nerd2nerd.org>
2020-05-26 13:38:21 +0200

    lower the timeout to switch to serial communication

--- Src/main.c
+++ Src/main.c
@@ -342,7 +342,7 @@ static void MX_TIM17_Init(void)
   /* USER CODE END TIM17_Init 1 */
   TIM_InitStruct.Prescaler = 15999;
   TIM_InitStruct.CounterMode = LL_TIM_COUNTERMODE_UP;
-  TIM_InitStruct.Autoreload = 3000;
+  TIM_InitStruct.Autoreload = 500;
   TIM_InitStruct.ClockDivision = LL_TIM_CLOCKDIVISION_DIV1;
   TIM_InitStruct.RepetitionCounter = 0;
   LL_TIM_Init(TIM17, &TIM_InitStruct);
